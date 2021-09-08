4859a449fce5cc2c8896ce40c40d9595d9f50454
ludekj <46579450+ludekj@users.noreply.github.com>
2021-09-09 00:10:08 +0200

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -9,3 +9,17 @@ for line in f.splitlines():
     if not line.startswith('#'):
         a.write(("/ip firewall address-list add address=") +
                 line + (" comment=blacklist list=blacklist\n"))
+
+input_file = "blacklist.rsc"
+with open(input_file, "r") as fp:
+    lines = fp.readlines()
+    new_lines = []
+    for line in lines:
+        # - Strip white spaces
+        line = line.strip()
+        if line not in new_lines:
+            new_lines.append(line)
+
+output_file = "blacklist.rsc"
+with open(output_file, "w") as fp:
+    fp.write("\n".join(new_lines))
